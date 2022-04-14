cb99a785dd70eea87a855f799da4f5e2131aa3ad
skyshr <kiyoungsuh@naver.com>
2022-04-14 15:05:28 +0900

    test

--- codingtest.js
+++ codingtest.js
@@ -40,22 +40,23 @@ function sum(absolutes, signs) {
 function primeCount(array) {
     function isPrime(n) {
         if (n==2 || n==3) return true
-        if (n==4) return false
-        for (let i = 3; i < parseInt(n/2) + 1; i++) {
+        if (n%2==0) return false
+        for (let i = 3; i < parseInt(n/2) + 1; i+=2) {
             if (n%i==0) return false
         }
         return true
     }
     var iterator = comb(array,3);
-    let result = 0;
+    let result = [];
     for (var item of iterator){
         let sum = item[0] + item[1] + item[2];
-        if (isPrime(sum)) result++
+        if (isPrime(sum)) {
+            if (!result.includes(sum)) result.push(sum)
+        }
     };
-
-    return result
+    return result.length
 }
 
-// console.log(primeCount([1,2,3,4]));
-// console.log(primeCount([1,2,7,6,4]));
-// console.log(primeCount([1,2,3,4,5]));
+console.log(primeCount([1,2,3,4]));
+console.log(primeCount([1,2,7,6,4]));
+console.log(primeCount([1,2,3,4,5]));
